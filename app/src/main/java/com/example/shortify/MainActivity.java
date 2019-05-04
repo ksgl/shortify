8cacf8a123f960433adf4a1dc3f41ce073544fe2
ksgl <ksenia.glazacheva@gmail.com>
2019-05-04 21:05:01 +0300

    <2> Элементы добавлены. Начало класса Input Processor

--- app/src/main/java/com/example/shortify/MainActivity.java
+++ app/src/main/java/com/example/shortify/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.shortify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button shortenBtn = findViewById(R.id.shorten_btn);
+
+        shortenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast toast = Toast.makeText(v.getContext(), "ah!", Toast.LENGTH_SHORT);
+//                toast.show();
+            }
+        });
     }
 }
