5eea40849e386377f625ec410625b63d7759744c
ksgl <ksenia.glazacheva@gmail.com>
2019-05-10 18:01:28 +0300

    latest

--- app/src/main/java/com/example/shortify/http/POST.java
+++ app/src/main/java/com/example/shortify/http/POST.java
@@ -1,8 +1,5 @@
 package com.example.shortify.http;
 
-import android.os.AsyncTask;
-import android.util.Log;
-
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -13,42 +10,22 @@ public class POST {
 
     OkHttpClient client = new OkHttpClient();
 
-    public void post(String json) {
-        new postAsyncTask(client).execute(json);
-    }
+    public String post(String json) {
 
-    public void showResponse(String response) {
-    }
-
-    private static class postAsyncTask extends AsyncTask<String, Void, String> {
-        private OkHttpClient c;
-
-        postAsyncTask(OkHttpClient client) {
-            c = client;
-        }
-
-        @Override
-        protected String doInBackground(String... json) {
-//            RequestBody body = RequestBody.create(JSON, json[0]);
-            String temp = "https://api-ssl.bitly.com/v3/shorten?access_token=fc11278ca50671dbd19332c8698026c7a9cd4123&longUrl=" + json[0];
-            Request request = new Request.Builder()
-                            .url(temp)
+        //            RequestBody body = RequestBody.create(JSON, json);
+        String temp = "https://api-ssl.bitly.com/v3/shorten?access_token=fc11278ca50671dbd19332c8698026c7a9cd4123&longUrl=" + json;
+        Request request = new Request.Builder()
+                .url(temp)
 //                            .post(body)
-                            .build();
+                .build();
 
 
-            try (Response response = c.newCall(request).execute()) {
-                return response.body().string();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return "";
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        @Override
-        protected void onPostExecute(String result) {
-            Log.i("response body = ", result);
-        }
+        return "";
     }
 }
