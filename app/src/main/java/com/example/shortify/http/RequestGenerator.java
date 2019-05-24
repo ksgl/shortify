0602b8e528d6b175b81235d4537a0baa35565f4d
Artem Andryukhov <mouseartiom@yandex.ru>
2019-05-24 21:04:01 +0300

    request generator

--- app/src/main/java/com/example/shortify/http/RequestGenerator.java
+++ app/src/main/java/com/example/shortify/http/RequestGenerator.java
@@ -1,4 +1,28 @@
 package com.example.shortify.http;
 
-public class RequestGenerator {
+import okhttp3.Request;
+
+// RequestGenerator generates API request counting on service selected
+abstract public class RequestGenerator {
+    String url = "";
+    String service = "";
+
+    public RequestGenerator() { }
+
+    public Request GetRequest(String service) {
+        String url = "";
+
+        switch (service) {
+            case "bit.ly":
+                url = "https://api-ssl.bitly.com/v3/shorten?access_token=fc11278ca50671dbd19332c8698026c7a9cd4123&longUrl=";
+                break;
+            case "cleanuri":
+                url = "https://cleanuri.com/api/v1/shorten";
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect service");
+        }
+
+        return new Request.Builder().url(url).build();
+    }
 }
