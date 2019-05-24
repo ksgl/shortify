4dff3db41f74d3e3de6466ebc2039f27ce0f648b
Artem Andryukhov <mouseartiom@yandex.ru>
2019-05-24 21:09:11 +0300

    fixed typos

--- app/src/main/java/com/example/shortify/MainActivity.java
+++ app/src/main/java/com/example/shortify/MainActivity.java
@@ -4,11 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-
 
 import com.example.shortify.history.HistoryActivity;
 import com.example.shortify.http.POST;
@@ -19,12 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Spinner spinner = findViewById(R.id.services_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.services, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
         Button shortenBtn = findViewById(R.id.shorten_btn);
 
@@ -45,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
 
                 } catch (Exception e) {
                     e.printStackTrace();
+                }
+            }
+        });
+
+        Button historyBtn = findViewById(R.id.history_btn);
+
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(v.getContext(), HistoryActivity.class);
+                startActivity(intent);
             }
         });
     }
