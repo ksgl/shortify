cd73e8ec8aa68b4df56be246ebc77f6a30826527
Artem Andryukhov <mouseartiom@yandex.ru>
2019-05-26 16:48:25 +0300

    added url viewer

--- app/src/main/java/com/example/shortify/history/HistoryActivity.java
+++ app/src/main/java/com/example/shortify/history/HistoryActivity.java
@@ -3,6 +3,7 @@ package com.example.shortify.history;
 import android.app.DatePickerDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -36,17 +37,20 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        viewModel = ViewModelProviders.of(this).get(LinkViewModel.class);
+        this.viewModel = ViewModelProviders.of(this).get(LinkViewModel.class);
 
-        viewModel.add(new LinkModel("yandex.ru","y.r", "23",true));
-        viewModel.add(new LinkModel("mmmmmmm.ru","m.r","55",true));
+        Context ctx = getApplicationContext();
+
+
+//        this.viewModel.add(new LinkModel("yandex.ru","y.r", "23",true));
+//        this.viewModel.add(new LinkModel("mmmmmmm.ru","m.r","55",true));
 
         recyclerView = findViewById(R.id.recycler_view);
         recyclerViewAdapter = new RecyclerViewAdapter(new ArrayList<LinkModel>());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(recyclerViewAdapter);
 
-        viewModel.getLinkList().observe(HistoryActivity.this, new Observer<List<LinkModel>>() {
+        this.viewModel.getLinkList().observe(HistoryActivity.this, new Observer<List<LinkModel>>() {
             @Override
             public void onChanged(@Nullable List<LinkModel> l) {
                 recyclerViewAdapter.addItems(l);
@@ -60,5 +64,12 @@ public class HistoryActivity extends AppCompatActivity {
                 viewModel.removeAll();
             }
         });
+    }
+
+    public void addLink(String longUrl, String shortUrl) {
+        Date date = new Date();
+        String strDate = date.toString();
+        this.viewModel.add(new LinkModel(longUrl,shortUrl, strDate, false));
+
     }
 }
