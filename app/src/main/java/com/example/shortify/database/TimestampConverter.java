37f227eeb7ef37426d50ff7defc39a3b0f5e25dc
Artem Andryukhov <mouseartiom@yandex.ru>
2019-05-28 01:05:35 +0300

    removed a lot of comments

--- app/src/main/java/com/example/shortify/database/TimestampConverter.java
+++ app/src/main/java/com/example/shortify/database/TimestampConverter.java
@@ -1,12 +1,6 @@
 package com.example.shortify.database;
 
 import android.arch.persistence.room.TypeConverter;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class TimestampConverter {
@@ -18,41 +12,5 @@ public class TimestampConverter {
     @TypeConverter
     public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
-//        DateFormat dateFormat = new SimpleDateFormat("dd MMM, HH:mm");
-//        String strDate = dateFormat.format(date);
-//        return strDate;
     }
-//    static private DateFormat df = new SimpleDateFormat("dd MMM, HH:mm");
-//    @TypeConverter
-//    public static Date toDate(String value) {
-//        try {
-//            return df.parse(value);
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    public static String toString(Date date) {
-//        return df.format(date);
-//    }
 }
-
-
-//    object TiviTypeConverters {
-//private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
-//
-//@TypeConverter
-//@JvmStatic
-//    fun toOffsetDateTime(value: String?): OffsetDateTime? {
-//            return value?.let {
-//            return formatter.parse(value, OffsetDateTime::from)
-//            }
-//            }
-//
-//@TypeConverter
-//@JvmStatic
-//    fun fromOffsetDateTime(date: OffsetDateTime?): String? {
-//            return date?.format(formatter)
-//            }
-//            }
